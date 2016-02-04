2514c2e80e0ffd64f8b557793606b0727ff3d945
Roger González <rogergonzalez21@gmail.com>
2016-02-04 18:43:55 -0430

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Roger Gonzalez'
+SITENAME = u'Gueb'
+#SITEURL = 'http://localhost:8000'
+SITEURL = 'http://gueb-designs.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Caracas'
+
+DEFAULT_LANG = u'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = 'stylish-template'
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
